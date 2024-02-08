efac7ac62ea728ff978350a2ce4bdd7eb2e4f71a
Ghaith <ghaithatrk93@gmail.com>
2024-02-08 20:29:16 +0800

    mobile view menu adjustment

--- src/components/MenuToggle.js
+++ src/components/MenuToggle.js
@@ -2,6 +2,7 @@ import { ThemeContext } from "@/context/ThemeContext";
 import { usePathname } from "next/navigation";
 import React, { useContext, useState } from "react";
 import { IoMdArrowDropdown } from "react-icons/io";
+import { IoCloseOutline } from "react-icons/io5";
 
 const MenuToggle = () => {
   const [openMenu, setOpenMenu] = useState(false);
@@ -12,18 +13,23 @@ const MenuToggle = () => {
 
   return (
     <div
-      className="relative block md:hidden border-[1px] border-black p-2 mx-1 rounded-full cursor-pointer"
+      className="block md:hidden border-[1px] border-black p-2 mx-1 rounded-full cursor-pointer"
       onClick={() => setOpenMenu(!openMenu)}
     >
       <IoMdArrowDropdown />
       <div
-        className="absolute top-[48px] right-0 bg-white border-2 border-black p-2 w-[140px]"
+        className="absolute top-16 left-1/2 transform -translate-x-1/2  bg-black/75 border-2 border-black text-white p-2 w-[320px] mx-auto"
         style={{ display: openMenu ? "block" : "none" }}
       >
-        <div className="flex flex-col items-center text-[20px]">
-          <a href="/" className="py-2">
-            Home
-          </a>
+        <div className="absolute right-3">
+          <IoCloseOutline fontSize={20} />
+        </div>
+        <div className="flex flex-col  h-full items-center text-[20px]">
+          {!isMain && (
+            <a href="/" className="py-2">
+              Home
+            </a>
+          )}
           <a href="/about" className="py-2">
             About Me
           </a>
