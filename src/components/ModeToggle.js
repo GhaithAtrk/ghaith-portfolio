5b15b5502249c6e35595f829f9b156db76efda4d
Ghaith <ghaithatrk93@gmail.com>
2024-02-09 09:32:21 +0800

    mode toggle adjustment

--- src/components/ModeToggle.js
+++ src/components/ModeToggle.js
@@ -3,14 +3,14 @@ import React, { useContext } from "react";
 import { FaRegLightbulb } from "react-icons/fa";
 
 const ModeToggle = () => {
-  const { toggle } = useContext(ThemeContext);
+  const { toggle, mode } = useContext(ThemeContext);
 
   return (
     <div
       className="border-[1px] border-black p-2 mx-1 rounded-full cursor-pointer"
       onClick={toggle}
     >
-      <FaRegLightbulb />
+      <FaRegLightbulb className={`${mode == "dark" ? `text-white` : null}`} />
     </div>
   );
 };
