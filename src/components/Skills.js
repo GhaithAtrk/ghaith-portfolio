451b8cc1d8d9de1f0d917b2e7dfd6077f4c89c3f
Ghaith <ghaithatrk93@gmail.com>
2024-04-29 14:23:24 +0800

    skills adjustments

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -29,12 +29,12 @@ const Skills = () => {
       label: "Next.js",
     },
     {
-      logo: "/skills/graphql.png",
-      label: "GraphQL",
-    },
-    {
       logo: "/skills/tailwind.png",
       label: "Tailwind",
+    },
+    {
+      logo: "/skills/graphql.png",
+      label: "GraphQL",
     },
     {
       logo: "/skills/bootstrap.png",
@@ -52,6 +52,10 @@ const Skills = () => {
       logo: "/skills/prismic.png",
       label: "Prismic",
     },
+    {
+      logo: "/skills/spline.png",
+      label: "Spline",
+    },
   ];
 
   return (
@@ -62,15 +66,15 @@ const Skills = () => {
       id="skills"
     >
       <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
-        <h2 className="text-4xl font-bold text-center md:text-left">
+        <h2 className="text-4xl my-4 font-bold text-center md:text-left">
           What Are My Skills?
         </h2>
-        <div className="grid grid-cols-2 lg:grid-cols-5 mb-10">
+        <div className="grid grid-cols-2 lg:grid-cols-6 mb-10">
           {skillsLogos.map((skill) => {
             return (
               <div
                 key={skill.label}
-                className="p-6 my-4 mx-2 bg-white/20 rounded-md hover:scale-105 ease-in duration-300"
+                className="p-4 m-2 bg-white/20 rounded-md hover:scale-105 ease-in duration-300"
               >
                 <div className="h-full flex flex-col justify-between gap-4 items-center">
                   <div className="m-auto">
